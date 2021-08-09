312262a7d6633bd391eab88ef05e65a6d48fc804
salman-mapkar <salmansmapkar@gmail.com>
2021-08-09 19:35:15 +0530

    Deleted Files

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    # Registration APIs
-    path('<str:username>/<str:password>/validate/', views.getAccountView.as_view({'get': 'list'}), name='getAccountView'),
-]
